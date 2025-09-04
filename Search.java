70bec160fdb381788c25c359afb05121f1bfdad1
Cayden0414 <caydenshih.03@gmail.com>
2025-09-03 20:00:24 -0700

    Added search.java

---
+++ Search.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Search {
+
+    HashMap hash;
+
+    public static Search(Hashmap hash){
+        this.hash = hash;
+    }
+
+    public static void BFS(Hashmap hash){
+        Queue<String> que = new Queue();
+        String[] searched;
+
+    }
+}
