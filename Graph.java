ee2bf6ed17a5d333b555e9c6200b2440bd83961d
Cayden0414 <caydenshih.03@gmail.com>
2025-09-10 19:57:28 -0700

    added graph.java

---
+++ Graph.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Graph {
+    List<String> lines;
+    List<Station> stations;
+
+    public Graph(){
+        this.lines = new ArrayList<String>();
+        this.stations = new ArrayList<Station>();
+    }
+
+    public void AddStationName(String name, String[] lines, Station previous, Station next){
+        // Creates the name of a new station and sets the previous and next station
+        Station newStation = new Station(name, lines);
+        newStation.setPrev(previous);
+        newStation.setNext(next);
+    }
+
+    public void AddLine(List<Station> stations, String line){
+        // Creates a new line and what stations it stops at
+        for (int i = 0; i < stations.size(); i++){
+
+            Station prev;
+            Station next;
+            if(i == 0){
+                prev = null;
+            }
+            else if(i == stations.size()-1){
+                next = null;
+            }else{
+                prev = stations.get(i-1);
+                next = stations.get(i+1);
+            }
+            AddStationName(stations.get(i).getName(), line, prev, next);
+        }
+    }
+
+    public List<Station> search(Station A, Station B){
+        return;
+    }
+}
--- Station.java
+++ Station.java
@@ -3,13 +3,30 @@ import java.util.*;
 public class Station {
     String name;
     String[] lines;
+    Station previous;
+    Station next;
     
     public static Station(String name, String[] lines){
         this.name = name;
         this.lines = lines;
+        
     }
 
-    public static String[] getLines(){
+    public String getName(){
+        return this.name;
+    }
+
+    public String[] getLines(){
         return this.lines;
     }
+
+    public void setPrev(Station previous){
+        this.previous = previous;
+    }
+
+    public void setNext(Station next){
+        this.next = next;
+    }
+
+    
 }
